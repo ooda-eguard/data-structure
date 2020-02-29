d17ff04e5a6c038039dd4cbb828a1e87d0b68d11
Conke Hu <conke.hu@maxwit.com>
2020-02-29 16:33:10 +0800

    Initial commit

---
+++ src/test/java/com/maxwit/AppTest.java
@@ -0,0 +1,17 @@
+package com.maxwit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        Assert.assertTrue(true);
+    }
+}
